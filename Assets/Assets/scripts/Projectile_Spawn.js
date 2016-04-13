2737e1aff792b2cfc5d6b558fb15b31000cc249f
JakeyCouper <WileECoyote2@users.noreply.github.com>
2016-04-13 09:23:48 -0700

    Initial commit

---
+++ Assets/Assets/scripts/Projectile_Spawn.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var projectile : Rigidbody;
+public var barrelEnd : Transform;
+
+function Update () {
+
+	if(Input.GetKeyDown("space")) 
+	{
+		var shot : Rigidbody;
+		shot = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation);
+		shot.AddForce(barrelEnd.right * 200);
+	}
+	
+}
